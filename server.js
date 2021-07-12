d2e60ce7883b5cf18090eaa3ea9df335450d080a
ubuntu <ubuntu@ubun2004.linuxvmimages.local>
2021-07-12 08:42:12 -0400

    First route, Query Blood bag

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 require ('dotenv').config();
 
+const fs = require('fs');
+const yaml = require('js-yaml');
+const { FileSystemWallet, Gateway } = require('fabric-network');
+
+const Blood = require('./contract/lib/blood');
+const wallet = new FileSystemWallet('./identity/user/isabella/wallet');
+
+
 
 const http = require('http');
 const bodyParser = require('body-parser');
@@ -23,6 +31,59 @@ app.get('/', function (req, res){
 
 app.set('port', PORT);
 
+app.route('/query/bag').get(async(req, res, next)=>{
+    const gateway = new Gateway();
+    try {
+        // Specify userName for network access
+        // const userName = 'isabella.issuer@magnetocorp.com';
+        const userName = 'User1@org1.example.com';
+
+        // Load connection profile; will be used to locate a gateway
+        let connectionProfile = yaml.safeLoad(fs.readFileSync('./gateway/networkConnection.yaml', 'utf8'));
+
+        // Set connection options; identity and wallet
+        let connectionOptions = {
+            identity: userName,
+            wallet: wallet,
+            discovery: { enabled:false, asLocalhost: true }
+        };
+
+        // Connect to gateway using application specified parameters
+        console.log('Connect to Fabric gateway.');
+        await gateway.connect(connectionProfile, connectionOptions);
+
+        // Access PaperNet network
+        console.log('Use network channel: mychannel.');
+
+        const network = await gateway.getNetwork('mychannel');
+        const contract = await network.getContract('bloodcontract');
+
+        console.log('--------------------Query Blood Bag--------------------');
+        const qBagResponse1 = await contract.evaluateTransaction('queryBloodBag', 'BD466:B+');
+        console.log(`${qBagResponse1.toString()}`);
+        console.log('Transaction complete.');
+
+        const output = qBagResponse1.toString()
+
+        res.status(200).json({
+            output
+        });
+    }
+     catch (error) {
+
+        console.log(`Error processing transaction. ${error}`);
+        console.log(error.stack);
+
+}   finally {
+        // Disconnect from the gateway
+        console.log('Disconnect from Fabric gateway.');
+        gateway.disconnect();
+
+}
+});
+
+
 httpServer.listen(PORT, () => {
     console.log('Server running on port', PORT);
 });
+
